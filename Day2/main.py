e32026b6c2d5f6ed4d0119119a9b48e4bff51f45
Abigail Uchenna Nkama <nkamaabigail1@gmail.com>
2023-01-14 00:14:46 +0100

    completed day2 of 100 day python coding

---
+++ Day2/main.py
@@ -0,0 +1,71 @@
+
+#EXERCISE 1
+"""Write a program that adds the digits
+in a 2 digit number. e.g. if the input
+was 35, then the output should be 3 + 5 = 8
+"""
+
+
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+
+two_digit_number = str(two_digit_number)
+a = int(two_digit_number[1])
+b = int(two_digit_number[0])
+print(a + b)
+#EXERCISE 2 - BMI Calculator
+"""Write a program that calculates the Body Mass Index (BMI) from a  user's weight and height"""
+# BMI = weight / height^2
+
+# 🚨 Don't change the code below 👇
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+BMI = int(weight) / (float(height) * float(height))
+print(round(BMI))
+
+
+#EXERCISE 3 - Life span calculator
+""" Create a program using maths and f-Strings
+    that tells us how many days, weeks, months and
+    years we have left if we live until 90 years old.
+    It will take your current age as the input and output
+    a message with our time left in this format:
+    You have x days, y weeks, and z months left.
+"""
+
+# 🚨 Don't change the code below 👇
+age = input("What is your current age? ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+Years_left = 90 - int(age)
+days_left = 365 * int(Years_left)
+weeks_left = 52 * int(Years_left)
+months_left = 12 * int(Years_left)
+
+print(f"You have {days_left} days, {weeks_left} weeks, {months_left} months and {Years_left} years left")
+
+
+
+#EXERCISE 4 - Tip Calculator
+"""
+    Creat a program that calculates the individual bill of each person
+    of a group of friends at a restaurant including the tip splited
+    amongs them.
+"""
+
+bill = float(input("Enter bill: "))
+tip = float(input("How much would you want to tip?(5%, 10%, 12%): "))
+num_of_people = int(input("For how many people?: "))
+tip_per_person = ((tip / 100) * bill) / num_of_people
+Each_person = (bill / num_of_people) + tip_per_person
+print(f"The bill for each person is {round(Each_person,2)}")
